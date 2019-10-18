9e3928411734f01058337cff33ebd3f5cbfeaf1f
Francois Van Laer <fvl209@nyu.edu>
2019-10-18 12:23:20 -0400

    Added secret_key in config.py

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ import os
 import sys
 from flask import Flask, render_template
 from flask_login import LoginManager
+from config import Config
 
 sys.path.append(os.path.dirname(__name__))
+
 app = Flask(__name__)
+app.config.from_object(Config)
 login = LoginManager(app)
 
 
