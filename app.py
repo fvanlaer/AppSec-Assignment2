5e018116a7c20dadd04ef255e6ea53d4cf07ace3
Francois Van Laer <fvl209@nyu.edu>
2019-10-18 12:01:27 -0400

    Added models.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import os
 import sys
 from flask import Flask, render_template
+from flask_login import LoginManager
 
 sys.path.append(os.path.dirname(__name__))
 app = Flask(__name__)
+login = LoginManager(app)
 
 
 @app.route('/')
