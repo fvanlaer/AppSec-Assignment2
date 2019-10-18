1ee5a50bad4bd898fc6ac3967c52af4902502016
Francois Van Laer <fvl209@nyu.edu>
2019-10-18 12:44:24 -0400

    Added password functions

--- app.py
+++ app.py
@@ -2,13 +2,19 @@ import os
 import sys
 from flask import Flask, render_template
 from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
 sys.path.append(os.path.dirname(__name__))
 
 app = Flask(__name__)
+# Getting configuration info from config.py
 app.config.from_object(Config)
+
 login = LoginManager(app)
+
+# Database
+db =  SQLAlchemy(app)
 
 
 @app.route('/')
