7005178cb431e409b77c36f0699bc1b3b6bceff6
Francois Van Laer <fvl209@nyu.edu>
2019-10-29 19:12:57 -0400

    Cleaning up

--- forms.py
+++ forms.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, ValidationError
-from models import User
 
 
 class LoginForm(FlaskForm):
