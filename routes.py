76bfb676002e91a26d052d53abd1ca888153e79f
Francois Van Laer <fvl209@nyu.edu>
2019-10-29 19:14:54 -0400

    Oops. Added utf-8 decode in spell_check()

--- routes.py
+++ routes.py
@@ -62,7 +62,7 @@ def spell_check():
         file.close()
         command = ["./spell_check", "input.txt", "wordlist.txt"]
         sub = subprocess.Popen(command, stdout=subprocess.PIPE)
-        misspelled = sub.communicate()[0].replace("\n", ", ")[:-2]
+        misspelled = sub.communicate()[0].decode("utf-8").replace("\n", ", ")[:-2]
         os.remove("input.txt")
         supplied_text = form.text_to_check.data
         form.text_to_check.data = ""
