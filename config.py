b5d5a362ff0c1c60a6643cb2dd3cdfd6b513a313
Francois Van Laer <fvl209@nyu.edu>
2019-12-12 16:47:20 -0500

    changing secret way (2)

--- config.py
+++ config.py
@@ -1,13 +1,11 @@
 import os
-from get_docker_secret import get_docker_secret
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
 
     # SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SECRET_KEY = open("/run/secrets/SECRET_KEY", "r").read().strip()
-    print(SECRET_KEY)
+    SECRET_KEY = open("/run/secrets/SECRET_KEY", "r").read().strip() or 'donotknow'
 
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
