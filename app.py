ed70b89ac99b412e11ccbb91826a72e9d4352f19
Francois Van Laer <fvl209@nyu.edu>
2019-12-12 17:21:49 -0500

    adding more secrets

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from config import Config
 from routes import blue
 from database import db
 from loginman import login_initializing
+from models import User
 
 sys.path.append(os.path.dirname(__name__))
 
@@ -18,6 +19,16 @@ def create_app():
         db.create_all()
     login_initializing(app)
     app.register_blueprint(blue, url_prefix='')
+
+    try:
+        master_password = open("/run/secrets/master_password", "r").read().strip()
+        master_phone = open("/run/secrets/master_phone", "r").read().strip()
+        admin = User(username='admin', phone=master_phone)
+        admin.set_password(master_password, master_phone)
+        db.session.add(admin)
+        db.session.commit()
+    except Exception:
+        pass
 
     return app
 
