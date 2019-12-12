a69293d8d9b29cd60d3e2a004e633e12ca95ac4b
Francois Van Laer <fvl209@nyu.edu>
2019-12-12 16:15:49 -0500

    Adding docker secrets

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,5 @@
 FROM python:3.6-alpine
 
-#RUN apt-get update -y
-#RUN apt-get install -y python3.6
-#RUN apt-get install -y python-pip python-dev
-
 COPY ./requirements.txt /app/requirements.txt
 
 WORKDIR /app
--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 import os
+from get_docker_secret import get_docker_secret
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
 
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    # SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = get_docker_secret('SECRET_KEY', default='donotknow')
 
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
